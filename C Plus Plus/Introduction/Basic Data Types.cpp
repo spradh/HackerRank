c4d2009cd002c76c1f0b90767d06043406dd04a5
Sabin <spradh@users.noreply.github.com>
2017-07-14 14:52:27 -0400

    Create Basic Data Types.cpp

---
+++ C Plus Plus/Introduction/Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int in;
+    long lo;
+    char ch;
+    float fl;
+    double dou;
+    scanf("%d %ld %c %f %lf",&in, &lo, &ch, &fl, &dou);
+    printf("%d\n%ld\n%c\n%f\n%lf",in, lo, ch, fl, dou);
+    return 0;
+}
