b3221eee0e9491b5c3220cbbc853fdd2849a2dac
Sabin <spradh@users.noreply.github.com>
2018-02-07 16:36:21 -0500

    Update 15 Days of Learning SQL.sql

--- SQL/Advanced Join/15 Days of Learning SQL.sql
+++ SQL/Advanced Join/15 Days of Learning SQL.sql
@@ -1,13 +1,36 @@
-select h1.hacker_id as hacker_id, h1.name as name,s.submission_date as submission_date, count(distinct s.submission_id) as num_of_submissions from hackers h1
-join (select s1.hacker_id as hacker_id from (
-                select submission_date, hacker_id 
-                    from submissions 
-                    group by submission_date,hacker_id) s1
-            group by s1.hacker_id
-            having count(distinct s1.submission_date)=(
-                select count(*) from (select count( distinct submission_date) from submissions 
-                    group by submission_date) s2)) uh
-    on uh.hacker_id=h1.hacker_id
-join submissions s 
-    on s.hacker_id=uh.hacker_id
-group by s.submission_date,h1.hacker_id,h1.name
+select t3.submission_date,
+    case 
+        when t3.high=1 then s5.hacker_id
+        else s6.hacker_id as hacker_id,
+   case 
+        when t3.high=1 then h2.hacker_name
+        else h3.hacker_name as hacker_name,
+        
+from (select s3.submission_date submission_date, max(s3.score) high,count(*) max_count 
+        from submissions s3 group by s3.submission_date) as t3 
+join
+    (select s4.submission_date submission_date, min(s4.score) low 
+        from submissions s4) as t4 
+    on t3.submission_date=s3.submission_date
+join 
+    submissions s5 
+    on s5.submission_date=t3.submission_date and s5.score=t3.high
+join submission s6
+    on s6.submission_date=t4.submission_date and s6.score=t4.low
+join hackers h2 
+    on h2.hacker_id=s5.hacker_id
+join hackers h3
+    on h3.hacker_id=s6.hacker_id
+where 
+    
+    
+    (select hf.hacker_id hacker_id from
+        (select t2.hacker_id as hacker_id from 
+            (select t1.submission_date as submission_date, t1.hacker_id as hacker_id 
+                from (select s1.submission_date as submission_date,s1.hacker_id as hacker_id 
+                    from submissions as s1, submissions as s2 
+                    where s2.hacker_id=s1.hacker_id and datediff(s1.submission_date,s2.submission_date)=1)as t1
+            group by submission_date, hacker_id) as t2
+        group by t2.hacker_id
+        having count(t2.submission_date)=14) as hf
+    join hackers h1 on h1.hacker_id=hf.hacker_id);
